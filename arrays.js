cfa34abd08dafacba6151c0dbabd91cbd8afad67
Paul Ashour <paul.ashour@gmail.com>
2018-09-09 15:50:05 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -11,5 +11,6 @@ function addElementToBeginningOfArray(array, element) {
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  array = 
+  array[1] = element;
+  return array
 }
