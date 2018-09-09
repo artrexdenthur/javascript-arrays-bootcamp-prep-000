b110a04073005ee71f2648da9c4d4be56f1d7f38
Paul Ashour <paul.ashour@gmail.com>
2018-09-09 16:14:40 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -46,5 +46,6 @@ function destructivelyRemoveElementFromEndOfArray(array) {
 }
 
 function removeElementFromEndOfArray(array) {
-  newArray = array.slice()
+  newArray = array.slice(0, array.length - 1);
+  return newArray;
 }
